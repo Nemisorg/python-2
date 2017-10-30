dc3f0ba8f878f73655ad939b0a65cb72b8a97d2d
Matthias Erselina <matthias.erselina@coderclass.nl>
2017-10-30 10:25:30 +0100

    Helemaal afgewerkt, bugs gefixt extra dicht gemaakt zodat er nooit een error komt, visuele galgen gemaakt en geimplanteerd, streepjes toegevoegd en dat de streepjes worden vervangen door goede letters.

--- hangman.py
+++ hangman.py
@@ -1,23 +1,99 @@
-import random
+def een():
+    print(" ___")
 
-def checkAllesGeraden(gw, gl):
-    print("Iets")
+def twee():
+    print("|")
+    print("|")
+    print("|")
+    print("|")
+    print("|")
+    print("|")
+    print("|___")
+
+def drie():
+    print("___________")
+    print("|/         |")
+    print("|")
+    print("|")
+    print("|")
+    print("|")
+    print("|")
+    print("|___")
+
+def vier():
+    print("___________")
+    print("|/         |")
+    print("|        (o o)")
+    print("|         /|\ ")
+    print("|         / \ ")
+    print("|        |___|")
+    print("|        |___|")
+    print("|___     |   |")
+
+def vijf():
+    print("___________")
+    print("|/         |")
+    print("|        (x x)")
+    print("|         /|\ ")
+    print("|         / \ ")
+    print("|")
+    print("|")
+    print("|___")
+
+def toongalg():
+    if count == 1:
+        een()
+    if count == 2:
+        twee()
+    if count == 3:
+        drie()
+    if count == 4:
+        vier()
+    if count == 5:
+        vijf()
+
+
+
 
 
 naam = input("Wat is je naam? ")
 print("Hallo " + naam + ", welkom bij galgje.")
 count = 0
-woordkeus = ("galgje", "downie", "laptop")
-word = (random.choice(woordkeus))
+word = input("Laat iemand een woord invoeren dat je moet raden: \n")
+while " " in word:
+    word = input("Voer één woord in!")
 goedeletters = ""
+streep = ["_ "] * len(word)
+alleletters = ("")
 while True:
-    streep = ("_ _ _ _ _ _")
-    print (streep)
-    letter = input("Geef een letter... ")
+    letter = input("Geef een letter. (Typ QQ om op te geven of '?' om te raden.): ")
+    if letter == "QQ":
+        vijf()
+        print("Je hebt jezelf aangegeven en je bent opgehangen.")
+        break
+
+    for letter2 in range(len(word)):
+        if letter is word[letter2]:
+            streep[letter2] = letter
+        print (streep[letter2], end='')
+    print('')
+
+    if "_ " not in streep:
+        print('je hebt gewonnen!')
+        toongalg()
+        break
     if letter in word:
-        print("Goed geraden!")
-        goedeletters = (goedeletters + letter)
-        print ("De letters " + goedeletters + " heb je goed.")
+        if len(letter) >= 2:
+            print("je hebt meer dan een letter gebruikt")
+        elif letter in goedeletters:
+            print ("Je hebt deze letter al genoemd. Noem een andere letter.")
+        else:
+            goedeletters = (goedeletters + letter)
+
+    elif letter in alleletters:
+        print ("Je hebt deze letter al genoemd. Noem een andere letter.")
+    elif len(letter) >= 2:
+        print("je hebt meer dan een letter gebruikt")
     elif letter == "?":
         wordguess = input("Probeer het woord maar te raden... ")
         if wordguess == word:
@@ -26,23 +102,18 @@ while True:
         else:
             print("Je hebt fout geraden!")
             print("Je hebt verloren!")
+            vijf()
             break
     elif letter in goedeletters:
         print ("Je hebt deze letter al genoemd. Noem een andere letter.")
-
-    elif checkAllesGeraden(goedeletters, word):
-        wordguess = input("Probeer het woord maar te raden... ")
-        if wordguess == word:
-            print("Je hebt het woord geraden!")
-            break
-        else:
-            print("Je hebt fout geraden!")
-            print("Je hebt verloren!")
-            break
     else:
         print("Fout")
         count = (count +1)
+        toongalg()
 
-    if count == 6:
+    if count == 5:
+        toongalg()
         print("Je hebt verloren!")
         break
+
+    alleletters = (alleletters + letter)
